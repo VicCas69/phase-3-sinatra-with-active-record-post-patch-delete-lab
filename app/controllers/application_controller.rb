a0bb8073b51b8972d93a6fbf3c3e37fe68820212
victor castro <victor_castro_01@hotmail.com>
2022-05-15 13:59:00 -0400

    post-patch-delete lab completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,30 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do 
+    baked_goods = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_goods.to_json
+  end
+
+  patch '/bakeries/:id' do
+    # find the bakery with the specified id
+    bakery = Bakery.find(params[:id])
+    # update the name of the bakery
+    bakery.update(
+      name: params[:name]
+    )
+    # return bakery in json format 
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do 
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
 end
